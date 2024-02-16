0533e174755e0d94708939662f98b8b520d4a5ca
Matt Stannett <matt.stannett@gmail.com>
2024-02-17 11:22:18 +1300

    Adds GraphQL support

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifytutorial": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "awsamplifytutorialdbd00b57": {
       "AppClientID": "string",
